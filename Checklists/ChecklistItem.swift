733e8f0b801ffe5e84c8f25c6bddb30358caf103
Brett Wallace <bwallace90@gmail.com>
2015-12-14 13:21:13 -0500

    Refactor model into array

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Brett Wallace on 12/14/15.
+//  Copyright © 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}

78708494762e1c28f8f8edbb5a1cbc7003776dd0
Brett Wallace <bwallace90@gmail.com>
2015-12-18 18:01:13 -0500

    Set up AddItemViewController file and hook up actions

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Brett Wallace on 12/18/15.
+//  Copyright © 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}

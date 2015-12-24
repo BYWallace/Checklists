129a7f981e98477f0302eee2c417698c58d9fab9
Brett Wallace <bwallace90@gmail.com>
2015-12-24 12:27:10 -0500

    Polish including disabling done/return when no characters

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -9,8 +9,9 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -29,5 +30,13 @@ class AddItemViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
         return nil
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text!
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        doneBarButton.enabled = (newText.length > 0)
+        return true
     }
 }

61383bc34634bf7c4167431256674c8c8f25e0c8
Brett Wallace <bwallace90@gmail.com>
2015-12-24 11:46:51 -0500

    Capture text field contents and present keyboard first

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,12 +10,20 @@ import Foundation
 import UIKit
 
 class AddItemViewController: UITableViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
     
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
         dismissViewControllerAnimated(true, completion: nil)
     }
     

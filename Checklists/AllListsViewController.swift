7d1df52c51d6a59349b7bebfb1398f0763e4de7e
Brett Wallace <bwallace90@gmail.com>
2016-01-14 22:24:40 -0500

    Add AllListsViewController and programatically add cells

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Brett Wallace on 1/14/16.
+//  Copyright © 2016 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = cellForTableView(tableView)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
+    }
+    
+    func cellForTableView(tableView: UITableView) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        if let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("ShowChecklist", sender: nil)
+    }
+}
